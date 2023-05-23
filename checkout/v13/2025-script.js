ff88d9da49c64b79996e71782f0692daea7db0f2
Piyush kalsariya <piyushkalasariya555@gmail.com>
2023-05-23 18:02:22 +0530

    checkout v13

--- checkout/v13/2025-script.js
+++ checkout/v13/2025-script.js
@@ -1,23 +1,23 @@
 (function () {
-  const cookieName = "#2025_AWR_Product_List_Popup";
-  const cookieValue = "1";
+  const cookieName = '#2025_AWR_Product_List_Popup';
+  const cookieValue = '1';
   const myDate = new Date();
 
   myDate.setDate(myDate.getDate() + 30);
-  document.cookie = cookieName + "=" + cookieValue + ";expires=" + myDate;
+  document.cookie = cookieName + '=' + cookieValue + ';expires=' + myDate;
 
   function createTest() {
-    if (navigator.userAgent.indexOf("Mac OS X") != -1) {
-      jQuery("body").addClass("mac");
+    if (navigator.userAgent.indexOf('Mac OS X') != -1) {
+      jQuery('body').addClass('mac');
     }
-    jQuery("body").addClass("spz-2025");
+    jQuery('body').addClass('spz-2025');
 
     const productsCheck = setInterval(async function () {
-      if (jQuery(".toggle").length == 0) {
-        await jQuery(".cart-note-div").remove();
-        await jQuery(".card-title").html(`<span>Your cart</span>`);
+      if (jQuery('.toggle').length == 0) {
+        await jQuery('.cart-note-div').remove();
+        await jQuery('.card-title').html(`<span>Your cart</span>`);
 
-        await jQuery(".padding-30px-0-0").removeClass("padding-30px-0-0");
+        await jQuery('.padding-30px-0-0').removeClass('padding-30px-0-0');
 
         // const button = await jQuery(".enroll-now.skiptranslate:not(.ga-track-upgrade-cart)");
 
@@ -35,7 +35,7 @@
         //   await jQuery("#checkout-btn").html(button);
         // // }
 
-        await jQuery(".ga-track-remove-product").html(
+        await jQuery('.ga-track-remove-product').html(
           `
           <svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M7.50008 1.94792C7.17042 1.94782 6.84884 2.04995 6.57964 2.24024C6.31045 2.43053 6.10689 2.69961 5.997 3.01042C5.94838 3.14105 5.85037 3.24735 5.7241 3.30638C5.59783 3.36541 5.45342 3.37246 5.322 3.32599C5.19059 3.27952 5.0827 3.18326 5.02161 3.05797C4.96051 2.93269 4.95111 2.78841 4.99541 2.65625C5.1787 2.13846 5.51796 1.6902 5.9665 1.37317C6.41504 1.05613 6.95081 0.885899 7.50008 0.885899C8.04935 0.885899 8.58512 1.05613 9.03366 1.37317C9.4822 1.6902 9.82146 2.13846 10.0047 2.65625C10.0491 2.78841 10.0397 2.93269 9.97856 3.05797C9.91746 3.18326 9.80958 3.27952 9.67816 3.32599C9.54675 3.37246 9.40233 3.36541 9.27606 3.30638C9.14979 3.24735 9.05178 3.14105 9.00317 3.01042C8.89319 2.69967 8.6896 2.43064 8.42043 2.24037C8.15125 2.05009 7.82972 1.94792 7.50008 1.94792ZM0.947998 4.25C0.947998 4.1091 1.00397 3.97398 1.1036 3.87435C1.20323 3.77472 1.33835 3.71875 1.47925 3.71875H13.5209C13.6618 3.71875 13.7969 3.77472 13.8966 3.87435C13.9962 3.97398 14.0522 4.1091 14.0522 4.25C14.0522 4.3909 13.9962 4.52602 13.8966 4.62565C13.7969 4.72528 13.6618 4.78125 13.5209 4.78125H1.47925C1.33835 4.78125 1.20323 4.72528 1.1036 4.62565C1.00397 4.52602 0.947998 4.3909 0.947998 4.25ZM3.18987 5.98542C3.18048 5.8448 3.11561 5.71368 3.00954 5.62089C2.90347 5.5281 2.76488 5.48125 2.62427 5.49065C2.48365 5.50004 2.35253 5.56491 2.25974 5.67098C2.16696 5.77705 2.12011 5.91564 2.1295 6.05625L2.45816 10.9806C2.51837 11.8887 2.56725 12.6225 2.682 13.1991C2.80171 13.7976 3.00429 14.2977 3.42362 14.6894C3.84225 15.0818 4.35508 15.2511 4.96071 15.3298C5.54296 15.4063 6.27821 15.4063 7.18912 15.4063H7.81175C8.72196 15.4063 9.45792 15.4063 10.0402 15.3298C10.6451 15.2511 11.1579 15.0818 11.5772 14.6894C11.9959 14.2977 12.1985 13.7969 12.3182 13.1991C12.4329 12.6225 12.4811 11.8887 12.542 10.9806L12.8707 6.05625C12.8801 5.91564 12.8332 5.77705 12.7404 5.67098C12.6476 5.56491 12.5165 5.50004 12.3759 5.49065C12.2353 5.48125 12.0967 5.5281 11.9906 5.62089C11.8846 5.71368 11.8197 5.8448 11.8103 5.98542L11.4845 10.8729C11.4207 11.827 11.3754 12.4915 11.2762 12.9908C11.1792 13.476 11.0446 13.7325 10.8512 13.9138C10.6571 14.0951 10.3922 14.2127 9.90204 14.2765C9.397 14.3423 8.73117 14.3438 7.77421 14.3438H7.22596C6.26971 14.3438 5.60387 14.3423 5.09812 14.2765C4.60796 14.2127 4.34304 14.0951 4.14896 13.9138C3.95558 13.7325 3.821 13.476 3.72396 12.9908C3.62479 12.4915 3.57946 11.827 3.51571 10.8729L3.18987 5.98542Z" fill="black"/>
@@ -44,16 +44,16 @@
           `
         );
 
-        await jQuery(".product-total-name").html(
+        await jQuery('.product-total-name').html(
           `
           <div>Your recurring monthly <br/>
           payment (including taxes)</div>
           `
         );
 
-        await jQuery(".pad-btn-checkout button").addClass("col-lg-5");
+        await jQuery('.pad-btn-checkout button').addClass('col-lg-5');
 
-        await jQuery(".payment-option").after(
+        await jQuery('.payment-option').after(
           `
           <label class="toggle">
             <span id="monthly" class="toggle-label monthly toggle-label-bold">Monthly</span>
@@ -64,22 +64,22 @@
           `
         );
 
-        $(document).on("change", "#toggle-input", (e) => {
+        $(document).on('change', '#toggle-input', (e) => {
           if (e.target.checked) {
-            jQuery(".annually").addClass("toggle-label-bold");
-            jQuery(".monthly").removeClass("toggle-label-bold");
-            jQuery("#frequency-Annual-desktop").click();
-            jQuery(".product-total-name").html(
+            jQuery('.annually').addClass('toggle-label-bold');
+            jQuery('.monthly').removeClass('toggle-label-bold');
+            jQuery('#frequency-Annual-desktop').click();
+            jQuery('.product-total-name').html(
               `
               <div>Your recurring annually <br/>
               payment (including taxes)</div>
               `
             );
           } else {
-            jQuery(".monthly").addClass("toggle-label-bold");
-            jQuery(".annually").removeClass("toggle-label-bold");
-            jQuery("#frequency-Monthly-desktop").click();
-            jQuery(".product-total-name").html(
+            jQuery('.monthly').addClass('toggle-label-bold');
+            jQuery('.annually').removeClass('toggle-label-bold');
+            jQuery('#frequency-Monthly-desktop').click();
+            jQuery('.product-total-name').html(
               `
               <div>Your recurring monthly <br/>
               payment (including taxes)</div>
@@ -89,8 +89,8 @@
         });
 
         if (
-          jQuery(".toggle").length > 0 ||
-          jQuery(".no-data-card-empty").length > 0
+          jQuery('.toggle').length > 0 ||
+          jQuery('.no-data-card-empty').length > 0
         ) {
           clearInterval(productsCheck);
         }
@@ -100,12 +100,11 @@
     }, 100);
 
     const checkoutBtnCheck = setInterval(async function () {
-      if (jQuery("#checkout-btn").length == 0) {
-        const button = await jQuery(
-          ".enroll-now.skiptranslate:not(.ga-track-upgrade-cart)"
-        );
-
-        await jQuery(".form-group.col-12.col-md-6:last").after(
+      const button = await jQuery(
+        '.enroll-now.skiptranslate:not(.ga-track-upgrade-cart)'
+      );
+      if (jQuery('#checkout-btn').length == 0 && button.length != 0) {
+        await jQuery('.form-group.col-12.col-md-6:last').after(
           `
             <div class="form-group col-12 col-md-6">
             <label style="opacity: 0">s</label>
@@ -114,20 +113,31 @@
             `
         );
         // if(jQuery('#checkout-btn .enroll-now.skiptranslate').length == 0) {
-        await jQuery("#checkout-btn").html(button);
+        await jQuery('#checkout-btn').html(button);
         // }
-        if (jQuery("#checkout-btn").length > 0) {
+        if (
+          jQuery(
+            '#checkout-btn .enroll-now.skiptranslate:not(.ga-track-upgrade-cart)'
+          ).length != 0
+        ) {
           clearInterval(checkoutBtnCheck);
         }
       } else {
-        clearInterval(checkoutBtnCheck);
+        if (
+          jQuery(
+            '#checkout-btn .enroll-now.skiptranslate:not(.ga-track-upgrade-cart)'
+          ).length != 0 ||
+          jQuery('.no-data-card-empty').length > 0
+        ) {
+          clearInterval(checkoutBtnCheck);
+        }
       }
     }, 100);
 
     const stepWapperCheck = setInterval(async function () {
-      if (jQuery(".form-inner-header").length == 0) {
-        await jQuery(".form-header").addClass("hide-mobile");
-        await jQuery(".form-header").html(
+      if (jQuery('.form-inner-header').length == 0) {
+        await jQuery('.form-header').addClass('hide-mobile');
+        await jQuery('.form-header').html(
           `
           <div class="stepper-wrapper">
             <div class="stepper-item completed">
@@ -154,7 +164,7 @@
         );
 
         await jQuery(
-          ".page-wrap.row.nav-wrap.justify-content-between.fs-restrict-dead-clicks"
+          '.page-wrap.row.nav-wrap.justify-content-between.fs-restrict-dead-clicks'
         ).after(
           `
             <div _ngcontent-c7="" class="form-header row m-0 hide-desktop">
@@ -183,7 +193,7 @@
             `
         );
 
-        await jQuery("#name").before(
+        await jQuery('#name').before(
           `
           <div class="form-inner-header">
             Your Personal Info
@@ -191,7 +201,7 @@
           `
         );
 
-        await jQuery("#email").after(
+        await jQuery('#email').after(
           `
           <div class="form-inner-header address">
           Your Address
@@ -199,15 +209,15 @@
           `
         );
 
-        await jQuery(".form-message").html(
+        await jQuery('.form-message').html(
           `
           All fields are required.
           `
         );
 
         if (
-          jQuery(".stepper-wrapper").length > 0 ||
-          jQuery(".no-data-card-empty").length > 0
+          jQuery('.stepper-wrapper').length > 0 ||
+          jQuery('.no-data-card-empty').length > 0
         ) {
           clearInterval(stepWapperCheck);
         }
@@ -217,9 +227,9 @@
     }, 100);
 
     const otherProductsCheck = setInterval(async function () {
-      const cartLinkDiv = await jQuery(".cart-link-div");
+      const cartLinkDiv = await jQuery('.cart-link-div');
       if (cartLinkDiv.length == 2) {
-        await jQuery(".ga-track-remove-product").html(
+        await jQuery('.ga-track-remove-product').html(
           `
           <svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M7.50008 1.94792C7.17042 1.94782 6.84884 2.04995 6.57964 2.24024C6.31045 2.43053 6.10689 2.69961 5.997 3.01042C5.94838 3.14105 5.85037 3.24735 5.7241 3.30638C5.59783 3.36541 5.45342 3.37246 5.322 3.32599C5.19059 3.27952 5.0827 3.18326 5.02161 3.05797C4.96051 2.93269 4.95111 2.78841 4.99541 2.65625C5.1787 2.13846 5.51796 1.6902 5.9665 1.37317C6.41504 1.05613 6.95081 0.885899 7.50008 0.885899C8.04935 0.885899 8.58512 1.05613 9.03366 1.37317C9.4822 1.6902 9.82146 2.13846 10.0047 2.65625C10.0491 2.78841 10.0397 2.93269 9.97856 3.05797C9.91746 3.18326 9.80958 3.27952 9.67816 3.32599C9.54675 3.37246 9.40233 3.36541 9.27606 3.30638C9.14979 3.24735 9.05178 3.14105 9.00317 3.01042C8.89319 2.69967 8.6896 2.43064 8.42043 2.24037C8.15125 2.05009 7.82972 1.94792 7.50008 1.94792ZM0.947998 4.25C0.947998 4.1091 1.00397 3.97398 1.1036 3.87435C1.20323 3.77472 1.33835 3.71875 1.47925 3.71875H13.5209C13.6618 3.71875 13.7969 3.77472 13.8966 3.87435C13.9962 3.97398 14.0522 4.1091 14.0522 4.25C14.0522 4.3909 13.9962 4.52602 13.8966 4.62565C13.7969 4.72528 13.6618 4.78125 13.5209 4.78125H1.47925C1.33835 4.78125 1.20323 4.72528 1.1036 4.62565C1.00397 4.52602 0.947998 4.3909 0.947998 4.25ZM3.18987 5.98542C3.18048 5.8448 3.11561 5.71368 3.00954 5.62089C2.90347 5.5281 2.76488 5.48125 2.62427 5.49065C2.48365 5.50004 2.35253 5.56491 2.25974 5.67098C2.16696 5.77705 2.12011 5.91564 2.1295 6.05625L2.45816 10.9806C2.51837 11.8887 2.56725 12.6225 2.682 13.1991C2.80171 13.7976 3.00429 14.2977 3.42362 14.6894C3.84225 15.0818 4.35508 15.2511 4.96071 15.3298C5.54296 15.4063 6.27821 15.4063 7.18912 15.4063H7.81175C8.72196 15.4063 9.45792 15.4063 10.0402 15.3298C10.6451 15.2511 11.1579 15.0818 11.5772 14.6894C11.9959 14.2977 12.1985 13.7969 12.3182 13.1991C12.4329 12.6225 12.4811 11.8887 12.542 10.9806L12.8707 6.05625C12.8801 5.91564 12.8332 5.77705 12.7404 5.67098C12.6476 5.56491 12.5165 5.50004 12.3759 5.49065C12.2353 5.48125 12.0967 5.5281 11.9906 5.62089C11.8846 5.71368 11.8197 5.8448 11.8103 5.98542L11.4845 10.8729C11.4207 11.827 11.3754 12.4915 11.2762 12.9908C11.1792 13.476 11.0446 13.7325 10.8512 13.9138C10.6571 14.0951 10.3922 14.2127 9.90204 14.2765C9.397 14.3423 8.73117 14.3438 7.77421 14.3438H7.22596C6.26971 14.3438 5.60387 14.3423 5.09812 14.2765C4.60796 14.2127 4.34304 14.0951 4.14896 13.9138C3.95558 13.7325 3.821 13.476 3.72396 12.9908C3.62479 12.4915 3.57946 11.827 3.51571 10.8729L3.18987 5.98542Z" fill="black"/>
@@ -228,108 +238,130 @@
           `
         );
 
-        await jQuery(".cart-other-link").html(
+        await jQuery('.cart-other-link').html(
           `Upgrade to our bundles for more savings!`
         );
 
-        await jQuery(".cart-link-div .cart-link").html("+ Upgrade");
+        await jQuery('.cart-link-div .cart-link').html('+ Upgrade');
 
         await jQuery(
-          ".cart-link-div .col-12.mar-15-0.flex-align-center .col-2.PD-0"
-        ).removeClass("col-2");
+          '.cart-link-div .col-12.mar-15-0.flex-align-center .col-2.PD-0'
+        ).removeClass('col-2');
         // await jQuery('.cart-link-div .col-12.mar-15-0.flex-align-center .col-2.PD-0').addClass('col-1');
 
         await jQuery(
-          ".cart-link-div .col-12.mar-15-0.flex-align-center .col-7.PR-0.text-align-center"
-        ).removeClass("col-7");
+          '.cart-link-div .col-12.mar-15-0.flex-align-center .col-7.PR-0.text-align-center'
+        ).removeClass('col-7');
         // await jQuery('.cart-link-div .col-12.mar-15-0.flex-align-center .col-7.PR-0.text-align-center').addClass('col-8');
 
-        await jQuery(
-          ".spz-2025 .cart-link-div .PR-0.text-align-center .margin-0.font-size-15"
-        ).html(
-          jQuery(
-            ".spz-2025 .cart-link-div .PR-0.text-align-center .margin-0.font-size-15"
-          )
-            .html()
-            .replaceAll("Add", "")
-        );
-
-        await jQuery(
-          ".spz-2025 .cart-link-div .PR-0.text-align-center .notranslate.margin-0.font-size-13"
-        ).html(
-          jQuery(
-            ".spz-2025 .cart-link-div .PR-0.text-align-center .notranslate.margin-0.font-size-13"
-          )
-            .html()
-            .replace("For only ", "For only<br/>")
-            .replaceAll("Per Month", "/mo")
-        );
-
-        await jQuery(".pad-btn-checkout button").addClass("col-lg-5");
-
-        const cardListComponent =
-          document.getElementsByClassName("cart-list")[0];
+        // const title = await jQuery(
+        //   ".cart-link-div .PR-0.text-align-center .margin-0.font-size-15"
+        // )
+        //   .html()
+        //   .replaceAll("Add", "")
 
         document
-          .getElementsByClassName("cart-list")[0]
-          .classList.add("mobile-only");
+          .querySelectorAll(
+            '.spz-2025 .cart-link-div .PR-0.text-align-center .margin-0.font-size-15'
+          )
+          .forEach((i) => {
+            i.innerHTML = i.innerHTML.replace('Add', '');
+          });
+
+        // await jQuery(
+        //   ".cart-link-div .PR-0.text-align-center .margin-0.font-size-15"
+        // ).html(
+        //   title
+        // );
+
+        // const amo = await jQuery(
+        //   ".spz-2025 .cart-link-div .PR-0.text-align-center .notranslate.margin-0.font-size-13"
+        // )
+        //   .html()
+        //   .replace("For only ", "For only<br/>")
+        //   .replaceAll("Per Month", "/mo")
+
+        document
+          .querySelectorAll(
+            '.spz-2025 .cart-link-div .PR-0.text-align-center .notranslate.margin-0.font-size-13'
+          )
+          .forEach((i) => {
+            i.innerHTML = i.innerHTML
+              .replace('For only ', 'For only<br/>')
+              .replace('Per Month', '/mo');
+          });
+
+        // await jQuery(
+        //   ".spz-2025 .cart-link-div .PR-0.text-align-center .notranslate.margin-0.font-size-13"
+        // ).html(
+        //   amo
+        // );
+
+        await jQuery('.pad-btn-checkout button').addClass('col-lg-5');
+
+        const cardListComponent =
+          document.getElementsByClassName('cart-list')[0];
+
+        document
+          .getElementsByClassName('cart-list')[0]
+          .classList.add('mobile-only');
         // await jQuery('.cart-link-div').after(
         //   cardListComponent
         // )
 
-        jQuery(".spz-2025 .cart-link-div .cart-link").on("click", () => {
+        jQuery('.spz-2025 .cart-link-div .cart-link').on('click', () => {
           setTimeout(() => {
             createTest();
           }, 1000);
         });
 
-        jQuery(".spz-2025 .ga-track-remove-product").on("click", () => {
+        jQuery('.spz-2025 .ga-track-remove-product').on('click', () => {
           setTimeout(() => {
             createTest();
           }, 1000);
         });
 
         if (
-          jQuery(".cart-other-link").html() ==
+          jQuery('.cart-other-link').html() ==
           `Upgrade to our bundles for more savings!`
         ) {
           clearInterval(otherProductsCheck);
         }
       } else {
-        clearInterval(otherProductsCheck);
+        // clearInterval(otherProductsCheck);
       }
     }, 100);
   }
 
   async function removeTest() {
-    jQuery("body").removeClass("spz-2025");
-    jQuery(".products-wrapper").remove();
-    jQuery(".noproducts-wrapper").remove();
-    jQuery(".form-header.row.m-0.hide-desktop").remove();
-    jQuery(".toggle").remove();
-    jQuery(".stepper-wrapper").remove();
+    jQuery('body').removeClass('spz-2025');
+    jQuery('.products-wrapper').remove();
+    jQuery('.noproducts-wrapper').remove();
+    jQuery('.form-header.row.m-0.hide-desktop').remove();
+    jQuery('.toggle').remove();
+    jQuery('.stepper-wrapper').remove();
   }
 
   history.pushState = (function (f) {
     return function pushState() {
       const ret = f.apply(this, arguments);
-      window.dispatchEvent(new Event("pushstate"));
-      window.dispatchEvent(new Event("locationchange"));
+      window.dispatchEvent(new Event('pushstate'));
+      window.dispatchEvent(new Event('locationchange'));
       return ret;
     };
   })(history.pushState);
   history.replaceState = (function (f) {
     return function replaceState() {
       const ret = f.apply(this, arguments);
-      window.dispatchEvent(new Event("replacestate"));
-      window.dispatchEvent(new Event("locationchange"));
+      window.dispatchEvent(new Event('replacestate'));
+      window.dispatchEvent(new Event('locationchange'));
       return ret;
     };
   })(history.replaceState);
-  window.addEventListener("popstate", function () {
-    window.dispatchEvent(new Event("locationchange"));
+  window.addEventListener('popstate', function () {
+    window.dispatchEvent(new Event('locationchange'));
   });
-  window.addEventListener("locationchange", function () {
+  window.addEventListener('locationchange', function () {
     url = location.href;
     urlCheck(url);
   });
@@ -340,8 +372,8 @@
   function urlCheck(url) {
     setTimeout(() => {
       if (
-        url.includes("user/checkout?section=cart") ||
-        url.includes("user/checkout?section=etspayment")
+        url.includes('user/checkout?section=cart') ||
+        url.includes('user/checkout?section=etspayment')
       ) {
         createTest();
       } else {
