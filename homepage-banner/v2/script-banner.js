5ba32ee54885c49d58e8c856078593d9eb517f2c
Piyush kalsariya <piyushkalasariya555@gmail.com>
2023-08-28 17:17:37 +0530

    v2

---
+++ homepage-banner/v2/script-banner.js
@@ -0,0 +1,80 @@
+(function () {
+  function createTest() {
+    if (navigator.userAgent.indexOf("Mac OS X") != -1) {
+      jQuery("body").addClass("mac");
+    }
+    jQuery("body").addClass("spz-2025");
+
+    const bannerCheck = setInterval(async function () {
+      const header = $(".top-bar");
+      header.append(`
+        <div class="new-banner">
+          <h2>NEW!</h2>
+          <p>
+            Protect your budget from costly repairs with our simple
+            <span class="bold-plan">whole home plans!</span>
+          </p>
+        </div>
+      `);
+      const banner = $(".new-banner");
+      banner.addClass("mt-32px");
+
+      clearInterval(bannerCheck);
+    }, 100);
+  }
+
+  async function removeTest() {
+    jQuery(".new-banner").remove();
+    jQuery("body").removeClass("spz-2025");
+  }
+
+  history.pushState = (function (f) {
+    return function pushState() {
+      const ret = f.apply(this, arguments);
+      window.dispatchEvent(new Event("pushstate"));
+      window.dispatchEvent(new Event("locationchange"));
+      return ret;
+    };
+  })(history.pushState);
+  history.replaceState = (function (f) {
+    return function replaceState() {
+      const ret = f.apply(this, arguments);
+      window.dispatchEvent(new Event("replacestate"));
+      window.dispatchEvent(new Event("locationchange"));
+      return ret;
+    };
+  })(history.replaceState);
+  window.addEventListener("popstate", function () {
+    window.dispatchEvent(new Event("locationchange"));
+  });
+  window.addEventListener("locationchange", function () {
+    urlCheck(location.href);
+    removeTest();
+  });
+
+  urlCheck(location.href);
+
+  $(window).on("scroll", () => {
+    const banner = $(".new-banner");
+    if ($(document).scrollTop() > 95) {
+      banner.removeClass("mt-32px");
+    } else {
+      banner.addClass("mt-32px");
+    }
+  });
+
+  function urlCheck(urla) {
+    if (
+      (urla.split("/").length <= 4 &&
+        urla.split("/")[urla.split("/").length - 1] !== "" &&
+        urla.split("/")[urla.split("/").length - 1].length > 0) ||
+      (urla.split("/")[urla.split("/").length - 1] === "" &&
+        urla.split("/")[urla.split("/").length - 2].length > 0)
+    ) {
+      console.log("callllllllll");
+      createTest();
+    } else {
+      removeTest();
+    }
+  }
+})();
